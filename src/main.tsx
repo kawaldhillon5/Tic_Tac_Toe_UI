81a8d203af8503a13317e655f905e45f1c313bdf
kawal <kawaldhillon5@gmail.com>
2026-01-15 09:17:08 -0500

    added router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './css/index.css'
+// We will create these components next
+import Root from './routes/root';
+import Game from './routes/game';
+import ErrorPage from './routes/error-page';
+import Home from './routes/home';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "game/:gameId", 
+        element: <Game />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
