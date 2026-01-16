5f17e430f6a4155e0bbb4f7d8ea7745c56a676a2
kawal <kawaldhillon5@gmail.com>
2026-01-15 23:46:01 -0500

    added GameProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './css/index.css'
-// We will create these components next
+
 import Root from './routes/root';
 import Game from './routes/game';
 import ErrorPage from './routes/error-page';
 import Home from './routes/home';
+import { GameProvider } from './hooks/useGame';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    element: <GameProvider><Root/></GameProvider>,
     errorElement: <ErrorPage />,
     children: [
       {
