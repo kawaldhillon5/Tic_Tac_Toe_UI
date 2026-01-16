b16cda9aed75f5d93ed997f9be6d2d2b8f1920be
kawal <kawaldhillon5@gmail.com>
2026-01-15 23:45:16 -0500

    moved ws logic to useGame

--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -1,22 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../api/axios';
-import { useSocket } from '../hooks/useSocket';
-import type { GameState } from '../types/game';
+import { useGameConext } from '../hooks/useGame';
 import '../css/game.css';
 
 export default function Game() {
     const { gameId } = useParams<{ gameId: string }>();
-    const [game, setGame] = useState<GameState | null>(null);
-    const [loading, setLoading] = useState(true);
-    const socket = useSocket(gameId);
 
-    // 1. Initial Load: Fetch the game from the SQLite DB
+    const {socket, username, game, setGame} = useGameConext();
+
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const fetchGame = async () => {
             try {
                 const response = await api.get(`/game/${gameId}`);
-                setGame(response.data.game);
+                setGame(response.data.game)
             } catch (err) {
                 console.error("Game not found", err);
             } finally {
@@ -24,32 +23,18 @@ export default function Game() {
             }
         };
         fetchGame();
-    }, [gameId]);
+    }, [gameId, socket]);
 
-    // 2. Real-time listener: Update state when the server emits 'game-updated'
-    useEffect(() => {
-        if (!socket) return;
-
-        socket.on('game-updated', (updatedGame: GameState) => {
-            setGame(updatedGame);
-        });
-
-        return () => { socket.off('game-updated'); };
-    }, [socket]);
-
-    // 3. Action: Send the move to the backend
     const handleCellClick = async (row: number, col: number) => {
         if (!game || game.status !== 'ongoing' || game.board[row][col]) return;
 
         try {
-            // We use the player from the current state as the one moving
             await api.post('/game/move', {
                 gameId: game.id,
                 row,
                 column: col,
                 player: game.current_turn
             });
-            // Note: We don't setGame here; we wait for the Socket to emit the update
         } catch (err: any) {
             alert(err.response?.data?.error || "Move failed");
         }
@@ -61,6 +46,7 @@ export default function Game() {
     return (
         <div className="game-container">
             <div className="game-info">
+                <h2>{username}</h2>
                 <h2>Game ID: {gameId?.slice(0, 8)}...</h2>
                 {game.status === 'ongoing' ? (
                     <p>Current Turn: <strong>{game.current_turn}</strong></p>
